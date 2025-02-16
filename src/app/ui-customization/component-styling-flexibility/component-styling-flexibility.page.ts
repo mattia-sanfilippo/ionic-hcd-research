f31724bbd762389b6a0f510ec88a5b12b4fd2142
Mattia Sanfilippo <me@mattiasanfilippo.dev>
2025-02-16 19:27:08 +0100

    feat: ui customization

--- src/app/ui-customization/component-styling-flexibility/component-styling-flexibility.page.ts
+++ src/app/ui-customization/component-styling-flexibility/component-styling-flexibility.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonButton, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonBackButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-component-styling-flexibility',
   templateUrl: './component-styling-flexibility.page.html',
   styleUrls: ['./component-styling-flexibility.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonBackButton],
 })
 export class ComponentStylingFlexibilityPage {
   constructor() {}
--- src/app/ui-customization/platform-consistency-vs-adaptation/platform-consistency-vs-adaptation.page.ts
+++ src/app/ui-customization/platform-consistency-vs-adaptation/platform-consistency-vs-adaptation.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-import { IonButton, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonBackButton, IonButtons, Platform } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-platform-consistency-vs-adaptation',
   templateUrl: './platform-consistency-vs-adaptation.page.html',
   styleUrls: ['./platform-consistency-vs-adaptation.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonBackButton, IonButtons, NgIf],
 })
-export class PlatformConsistencyVsAdaptationPage {
-  constructor() {}
+export class PlatformConsistencyVsAdaptationPage implements OnInit {
+
+  public isIos: boolean = false;
+  public isAndroid: boolean = false;
+
+  constructor(private platform: Platform) {}
+
+  ngOnInit() {
+    this.isIos = this.platform.is('ios');
+    this.isAndroid = this.platform.is('android');
+    console.log('Platform: ', this.platform.platforms());
+  }
 }
